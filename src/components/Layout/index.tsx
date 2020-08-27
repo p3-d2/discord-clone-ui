18a455c86fe1e8c7872e14c3cd9da70619837356
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-08-27 14:50:29 -0300

    creating and styling Server List component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import { Grid } from './styles';
 
+import ServerList from '../ServerList'
+
 const Layout: React.FC = () => {
   return (
-    <Grid />
+    <Grid>
+      <ServerList />
+    </Grid>
   );
 }
 
